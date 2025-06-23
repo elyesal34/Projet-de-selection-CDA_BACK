944dde268c1065cf5d24490f3b3b3ee7bf104304
Elyes <allanielyes34@gmail.com>
2025-06-23 09:31:46 +0200

    fix: reorder routes to ensure getJokeById is correctly defined

--- routes/jokeRoutes.js
+++ routes/jokeRoutes.js
@@ -4,8 +4,9 @@ const jokeController = require('../controllers/jokeController');
 
 router.post('/blagues', jokeController.addJoke);
 router.get('/blagues', jokeController.getAllJokes);
-router.get('/blagues/:id', jokeController.getJokeById);
 router.get('/blagues/random', jokeController.getRandomJoke);
+router.get('/blagues/:id', jokeController.getJokeById);
+
 
 router.put('/blagues/:id', jokeController.updateJoke);
 router.delete('/blagues/:id', jokeController.deleteJoke);
